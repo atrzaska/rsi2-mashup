b7c049700a8515e32cd2fa63a47f2b11247aa873
Andrzej Trzaska <atrzaska2@gmail.com>
2014-01-14 11:56:33 +0100

    cleanup EventDetailsServlet.java

--- src/main/java/org/atrzaska/rsi2/mashup/servlets/EventDetailsServlet.java
+++ src/main/java/org/atrzaska/rsi2/mashup/servlets/EventDetailsServlet.java
@@ -37,18 +37,5 @@ public class EventDetailsServlet extends HttpServlet {
         } catch (EVDBRuntimeException | EVDBAPIException ex) {
             Logger.getLogger(EventDetailsServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-//        response.setContentType("text/html;charset=UTF-8");
-//        try (PrintWriter out = response.getWriter()) {
-//            out.println("<!DOCTYPE html>");
-//            out.println("<html>");
-//            out.println("<head>");
-//            out.println("<title>Servlet NewServlet</title>");
-//            out.println("</head>");
-//            out.println("<body>");
-//            out.println("Event: " + event.getTitle());
-//            out.println("</body>");
-//            out.println("</html>");
-//        }
     }
 }
